a4ca7d002f3bc80f5d2017dd0b86df932c265ed8
Vandesm14 <vandesm14@hyperjs-servers.com>
2019-03-23 15:07:15 -0400

    main boot file

---
+++ os/boot.js
@@ -0,0 +1,11 @@
+/* jshint esversion: 6 */
+/* jshint -W027 */
+const fs = require('fs');
+const readlineSync = require('readline-sync');
+// const colors = require('colors/safe');
+const colors = require('ansi-colors');
+// const colors = require('chalk');
+
+__jnixdirname = __dirname + '/os';
+home = __jnixdirname + '/home';
+pwd = home;
