d7ab208053c1bbd6c987fd040afe543c0e803835
vandesm14 <vandesm14@gmail.com>
2019-12-09 15:13:51 -0500

    update frontend and dynamically updated pages

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const socketio = require('socket.io');
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
@@ -9,43 +10,44 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
+app.use(['/app/:q','/default/:q'], express.static('public'));
 app.set('view-engine', 'ejs');
 
+var apps = fs.readdirSync('modules');
+apps = apps.filter(el => el.match('.html'));
+apps = apps.map(el => el.replace('.html', ''));
+
 app.use((req, res, next) => {
-	res.locals.name = null;
+	res.locals.title = null;
 	res.locals.content = null;
 	res.locals.message = null;
+	res.locals.apps = apps;
 	next();
 })
 
 app.get('/', (req, res) => {
-	let file = fs.readFileSync('public/views/index.html');
-	res.render(__dirname + '/public/includes/templates/window.ejs', { content: file });
+	let file = fs.readFileSync('public/module/default/home.html');
+	res.render(__dirname + '/public/includes/templates/window.ejs', { content: file, apps });
 });
 
 app.get('/new', (req, res) => {
-	let q = req.query.q;
-	let file;
-	let apps = fs.readdirSync('modules');
-	apps = apps.map(el => el.replace('.html', ''));
-	if (q) {
-		file = fs.readFileSync(`modules/${q}.html`, 'utf8');
-		res.render(__dirname + '/public/includes/templates/window.ejs', { content: file, title: q });
-	} else {
-		ejs.renderFile(__dirname + '/public/views/new.ejs', { apps }, (err, str) => {
-			res.render(__dirname + '/public/includes/templates/window.ejs', { content: str, title: q });
-		});
-	}
+	ejs.renderFile(__dirname + '/public/views/new.ejs', { apps }, (err, str) => {
+		res.render(__dirname + '/public/includes/templates/window.ejs', { content: str });
+	});
 });
 
 app.get('/app/:q', (req, res) => {
 	let q = req.params.q;
 	let file;
-	let apps = fs.readdirSync('modules');
-	apps = apps.map(el => el.replace('.html', ''));
-	ejs.renderFile(__dirname + '/public/views/new.ejs', { apps }, (err, str) => {
-		res.render(__dirname + '/public/includes/templates/window.ejs', { content: str, title: q });
-	});
+	file = fs.readFileSync(`modules/${q}.html`, 'utf8');
+	res.render(__dirname + '/public/includes/templates/window.ejs', { content: file, title: q });
+});
+
+app.get('/default/:q', (req, res) => {
+	let q = req.params.q;
+	let file;
+	file = fs.readFileSync(`modules/default/${q}.html`, 'utf8');
+	res.render(__dirname + '/public/includes/templates/window.ejs', { content: file, title: q });
 });
 
 app.listen(3000, () => console.log('server started'));
